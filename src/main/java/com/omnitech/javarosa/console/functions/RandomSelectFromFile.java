81fd162564bcadbdb7b321a7e70ebcb837c9bc22
kayr <noryak1234@gmail.com>
2017-12-28 19:12:54 +0300

    support for random-select-from-file()

--- src/main/java/com/omnitech/javarosa/console/functions/RandomSelectFromFile.java
+++ src/main/java/com/omnitech/javarosa/console/functions/RandomSelectFromFile.java
@@ -13,6 +13,8 @@ import java.util.stream.Collectors;
 
 public class RandomSelectFromFile implements ISimpleFunctionHandler {
 
+    public static final String AUTO_FILL_BASE_PATH = "autofill.base.path";
+
     @Override
     public List<Class[]> getPrototypes() {
         Class[]       classes    = {String.class};
@@ -28,9 +30,15 @@ public class RandomSelectFromFile implements ISimpleFunctionHandler {
 
     @Override
     public Object evalImpl(Object[] args, EvaluationContext ec) throws Exception {
+        String basePathEnv  = System.getenv(AUTO_FILL_BASE_PATH);
+        String basePathProp = System.getProperty(AUTO_FILL_BASE_PATH);
+        String basePath     = basePathEnv != null ? basePathEnv : basePathProp;
+
         String       filePath = XPathFuncExpr.toString(args[0]);
-        Path         path     = Paths.get(filePath);
+        Path         path     = basePath != null ? Paths.get(basePath, filePath) : Paths.get(filePath);
         List<String> lines    = Files.lines(path).collect(Collectors.toList());
+
+
         return FormUtils.getRandom(lines);
     }
 
--- src/test/groovy/com/omnitech/javarosa/console/functions/RandomSelectFromFileTest.groovy
+++ src/test/groovy/com/omnitech/javarosa/console/functions/RandomSelectFromFileTest.groovy
@@ -16,5 +16,27 @@ class RandomSelectFromFileTest implements SmallFormEvaluator {
 
         GroovyAssert.shouldFailWithCause(NoSuchFileException) { evalXpath("random-select-from-file('fake-file-path')") }
 
+
+        setAutoFillBasePath("bad-base-path")
+        GroovyAssert.shouldFailWithCause(NoSuchFileException) {
+            evalXpath("random-select-from-file('src/test/resources/data/options.txt')")
+        }
+        resetBasePath()
+
+        setAutoFillBasePath(".")
+        assert evalXpath("random-select-from-file('src/test/resources/data/options.txt')")
+        resetBasePath()
+
+
     }
+
+    private String setAutoFillBasePath(String bas) {
+        return System.setProperty(RandomSelectFromFile.AUTO_FILL_BASE_PATH, bas)
+    }
+
+    private void resetBasePath() {
+        System.properties.remove(RandomSelectFromFile.AUTO_FILL_BASE_PATH)
+    }
+
+
 }
