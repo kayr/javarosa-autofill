3703497550a395503b957868b81152e6d92bf944
kayr <noryak1234@gmail.com>
2018-02-16 09:37:13 +0300

    move js files to lib folder

--- web/src/main/resources/web/js/lib/bootbox.js
+++ web/src/main/resources/web/js/lib/bootbox.js
@@ -11,12 +11,12 @@
   "use strict";
   if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
-    define(["jquery"], factory);
+    define(["web/js/lib/jquery"], factory);
   } else if (typeof exports === "object") {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require("jquery"));
+    module.exports = factory(require("web/js/lib/jquery"));
   } else {
     // Browser globals (root is window)
     root.bootbox = factory(root.jQuery);
